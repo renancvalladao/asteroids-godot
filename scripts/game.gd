8009129866cf1aa443700975c53c8d67b1542e7f
Renan Valladão <renancvalladao@gmail.com>
2023-04-26 20:44:33 -0300

    Adding reset button

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,5 +6,9 @@ extends Node2D
 func _ready():
 	player.connect("laser_shot", _on_player_laser_shot)
 
+func _process(_delta):
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
 func _on_player_laser_shot(laser):
 	lasers.add_child(laser)
