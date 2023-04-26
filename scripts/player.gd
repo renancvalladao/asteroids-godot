c06f13ffa9457ab1a4952c1e65389846115793d2
Renan Valladão <renancvalladao@gmail.com>
2023-04-26 17:33:06 -0300

    Shooting lasers

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,25 @@
 extends CharacterBody2D
 
+signal laser_shot(laser)
+
 @export var acceleration := 10
 @export var max_speed := 350
 @export var rotation_speed := 250
+
+@onready var muzzle = $Muzzle
+
+var laser_scene = preload("res://scenes/laser.tscn")
+
+var shoot_cd = false
+var rate_of_fire = 0.15
+
+func _process(_delta):
+	if Input.is_action_pressed("shoot"):
+		if !shoot_cd:
+			shoot_cd = true
+			shoot_laser()
+			await get_tree().create_timer(rate_of_fire).timeout
+			shoot_cd = false
 
 func _physics_process(delta):
 	var input_vector := Vector2(0, Input.get_axis("move_forward", "move_backward"))
@@ -30,3 +47,9 @@ func _physics_process(delta):
 		global_position.x = screen_size.x
 	elif global_position.x > screen_size.x:
 		global_position.x = 0
+
+func shoot_laser():
+	var l = laser_scene.instantiate()
+	l.global_position = muzzle.global_position
+	l.rotation = rotation
+	emit_signal("laser_shot", l)
