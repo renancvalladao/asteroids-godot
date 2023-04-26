891bdbafd57c84dffc5071a9b9e51e9bb76a10e0
Renan Valladão <renancvalladao@gmail.com>
2023-04-26 14:03:02 -0300

    Adding player controller

---
+++ scenes/scripts/player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+@export var acceleration := 10
+@export var max_speed := 350
+@export var rotation_speed := 250
+
+func _physics_process(delta):
+	var input_vector := Vector2(0, Input.get_axis("move_forward", "move_backward"))
+
+	velocity += input_vector.rotated(rotation) * acceleration
+	velocity = velocity.limit_length(max_speed)
+
+	if Input.is_action_pressed("rotate_right"):
+		rotate(deg_to_rad(rotation_speed * delta))
+	if Input.is_action_pressed("rotate_left"):
+		rotate(deg_to_rad(-rotation_speed * delta))
+
+	if input_vector.y == 0:
+		velocity = velocity.move_toward(Vector2.ZERO, 3)
+
+	move_and_slide()
+
+	var screen_size = get_viewport_rect().size
+	if global_position.y < 0:
+		global_position.y = screen_size.y
+	elif global_position.y > screen_size.y:
+		global_position.y = 0
+		
+	if global_position.x < 0:
+		global_position.x = screen_size.x
+	elif global_position.x > screen_size.x:
+		global_position.x = 0
